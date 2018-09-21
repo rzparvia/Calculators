8a21dbfe4938f219f8c3b8d432883d37e3702e0c
rzparvia <arno.p.parviainen@gmail.com>
2018-09-21 11:32:15 +0300

    UUtta

--- src/Binaari.java
+++ src/Binaari.java
@@ -7,7 +7,7 @@ public class Binaari {
         int luku1, luku2;
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Anna kaksi binaarilukua\nAnna ensimmäinen binaariluku:");
+        System.out.println("Anna kaksi binaarilukua\nAnna ensimmäinen binaariluku!!!!:");
         luku1 = sc.nextInt();
         System.out.println("Anna toinen binaariluku:");
         luku2 = sc.nextInt();
